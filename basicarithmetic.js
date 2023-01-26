119d79f30a5829736cb0b97bd81bbbf5245366f9
Sachin Tewari <mailforsachin@gmail.com>
2023-01-25 20:05:51 -0500

    write

---
+++ basicarithmetic.js
@@ -0,0 +1,18 @@
+// Print even
+let i=1;
+while (i <12)
+{
+    if (i%2==0)
+    {
+        document.write(i)
+        document.write("<br>")
+    }
+    i=i+1;
+
+}
+
+for(let i=1;i<10;i++)
+{
+    document.write(`'#'* ${i}`)
+    document.write("<br>")
+}
